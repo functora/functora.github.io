a4e7fe6fc01346a432debe4bb82c008c81e1c1ed
functora <functora@proton.me>
2022-12-25 18:43:53 -0300

    OpenGL

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -28,8 +28,20 @@ in
       TERMINAL = "alacritty";
     };
     nixpkgs.config.allowUnfree = true;
+    #
+    # Media
+    #
     sound.enable = true;
     hardware.pulseaudio.enable = true;
+    hardware.opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+    };
     #
     # Home
     #

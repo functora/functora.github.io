662108993893323e925d2c4edc010ea89632dec3
functora <functora@proton.me>
2022-12-26 18:27:34 -0300

    i3 WIP

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -55,6 +55,9 @@ in
         alacritty
         qutebrowser
         (import ./yewtube.nix {inherit pkgs;})
+        xorg.xev
+        dmenu
+        i3lock
       ];
       programs.git = {
         enable = true;
@@ -64,6 +67,121 @@ in
       home.file = {
         ".config/qutebrowser/config.py".source = ../cfg/qutebrowser.py;
         ".config/mps-youtube/config.json".source = ../cfg/yewtube.json;
+      };
+      programs.i3status-rust = {
+        enable = true;
+        bars.bottom.blocks = [
+          {
+            block = "battery";
+            format = " BAT {percentage} {time}";
+            full_format = " BAT: FULL";
+            icons_format = "";
+          }
+          {
+            block = "backlight";
+            format = " BLT {brightness}";
+            icons_format = "";
+          }
+          {
+            block = "networkmanager";
+          }
+          {
+            block = "disk_space";
+            info_type = "available";
+            format = "SSD{available}";
+            alert = 10.0;
+            warning = 20.0;
+            unit = "GB";
+            icons_format = "";
+          }
+          {
+            block = "memory";
+            display_type = "memory";
+            format_mem = " RAM{mem_used_percents}";
+            clickable = false;
+            icons_format = "";
+          }
+          #
+          # Uncomment if swap is used
+          #
+          #{
+          #  block = "memory";
+          #  display_type = "swap";
+          #  format_swap = " SWP {swap_used_percents}";
+          #  clickable = false;
+          #  icons_format = "";
+          #}
+          {
+            block = "cpu";
+            format = " CPU {utilization}";
+            icons_format = "";
+          }
+          {
+            block = "sound";
+          }
+          {
+            block = "time";
+            format = " %a %Y-%m-%d %R";
+            interval = 60;
+            icons_format = "";
+          }
+        ];
+      };
+      xsession.enable = true;
+      xsession.windowManager.i3 = {
+        enable = true;
+        config = let mod = "Mod4"; in {
+          modifier = mod;
+          keybindings = with pkgs; lib.mkOptionDefault {
+            "XF86AudioRaiseVolume" =
+              "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +5%";
+            "Mod5+Shift+XF86AudioRaiseVolume" =
+              "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +5%";
+
+            "XF86AudioLowerVolume" =
+              "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -5%";
+            "Mod5+Shift+XF86AudioLowerVolume" =
+              "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -5%";
+
+            "XF86AudioMute" =
+              "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
+            "Mod5+Shift+XF86AudioMute" =
+              "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
+
+            "XF86AudioMicMute" =
+              "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+            "Mod5+Shift+XF86AudioMicMute" =
+              "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+
+            "XF86AudioPlay" = "exec ${playerctl}/bin/playerctl play-pause";
+            "XF86AudioNext" = "exec ${playerctl}/bin/playerctl next";
+            "XF86AudioPrev" = "exec ${playerctl}/bin/playerctl previous";
+            "Mod5+Shift+XF86AudioPlay" = "exec ${playerctl}/bin/playerctl play-pause";
+            "Mod5+Shift+XF86AudioNext" = "exec ${playerctl}/bin/playerctl next";
+            "Mod5+Shift+XF86AudioPrev" = "exec ${playerctl}/bin/playerctl previous";
+
+            "XF86MonBrightnessDown" = "exec '${brightnessctl}/bin/brightnessctl s 10-'";
+            "XF86MonBrightnessUp" = "exec '${brightnessctl}/bin/brightnessctl s +10'";
+            "Mod5+Shift+XF86MonBrightnessDown" = "exec '${brightnessctl}/bin/brightnessctl s 10-'";
+            "Mod5+Shift+XF86MonBrightnessUp" = "exec '${brightnessctl}/bin/brightnessctl s +10'";
+          };
+          bars = [{
+            statusCommand =
+              "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
+          }];
+          startup = [
+            #{
+            #  command = "exec i3-msg workspace 1";
+            #  always = true;
+            #  notification = false;
+            #}
+            #{
+            #  command = "systemctl --user restart polybar.service";
+            #  always = true;
+            #  notification = false;
+            #}
+          ];
+        };
       };
     };
     #
@@ -92,21 +210,11 @@ in
       # GUI
       #
       enable = true;
-      desktopManager = {
-        xterm.enable = false;
-      };
+      windowManager.i3.enable = true;
+      desktopManager.xterm.enable = false;
       displayManager = {
         defaultSession = "none+i3";
         sessionCommands = "${pkgs.xorg.xmodmap}/bin/xmodmap ${xkb}";
-      };
-      windowManager.i3 = {
-        enable = true;
-        extraPackages = with pkgs; [
-          dmenu
-          i3status
-          i3lock
-          i3blocks
-        ];
       };
     };
   };

ba4c78e3f894769a54e112bc446c665b6cc4d5bf
functora <functora@proton.me>
2023-01-02 10:23:30 -0300

    some shell tools

--- nix/shell.nix
+++ nix/shell.nix
@@ -2,11 +2,8 @@ with (import ./project.nix);
 shellFor {
   exactDeps = true;
   withHoogle = false;
-  #buildInputs = [
-  #  pkgs.haskell-language-server
-  #];
-  #tools = {
-  #  cabal = "latest";
-  #  hlint = "latest";
-  #};
+  buildInputs = [
+    pkgs.cabal-install
+    pkgs.hlint
+  ];
 }

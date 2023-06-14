02e06d1b74e891ff9836e6765fb420fed3c7a6dc
functora <functora@proton.me>
2023-06-13 21:31:25 -0300

    rigtora

--- nix/rigtora.nix
+++ nix/rigtora.nix
@@ -6,7 +6,8 @@ let
   # wifiPwd = "TODO";
   xmrAddr = config.services.rigtora.xmrAddr;
   xmrSolo = config.services.rigtora.xmrSolo;
-in {
+in
+{
   imports = [
 
   ];
@@ -17,41 +18,45 @@ in {
     };
     xmrSolo = mkOption {
       type = types.bool;
+      default = false;
     };
   };
 
-  # networking = {
-  #   hostName = machine;
-  #   wireless = {
-  #     enable = true;
-  #     networks."${wifiNet}".psk = wifiPwd;
-  #     interfaces = [ "wlan0" ];
-  #   };
-  # };
+  config = {
+    # networking = {
+    #   hostName = machine;
+    #   wireless = {
+    #     enable = true;
+    #     networks."${wifiNet}".psk = wifiPwd;
+    #     interfaces = [ "wlan0" ];
+    #   };
+    # };
 
-  nix.settings.auto-optimise-store = true;
-  environment.systemPackages = with pkgs; [ vim htop ];
-  services.journald.extraConfig = ''
-    SystemMaxUse=100M
-    MaxFileSec=7day
-  '';
+    boot.cleanTmpDir = true;
+    nix.settings.auto-optimise-store = true;
+    environment.systemPackages = with pkgs; [ vim htop ];
+    services.journald.extraConfig = ''
+      SystemMaxUse=100M
+      MaxFileSec=7day
+    '';
 
-  services.fail2ban.enable = true;
-  services.openssh.enable = true;
-  services.xmrig.enable = true;
-  services.xmrig.settings = {
-    autosave = true;
-    opencl = false;
-    cuda = false;
-    cpu = true;
-    pools = [{
-      url = "pool.hashvault.pro:80";
-      coin = "XMR";
-      user = if xmrSolo then "solo:${xmrAddr}" else xmrAddr;
-      nicehash = false;
-      keepalive = false;
-      tls = true;
-      tls-fingerprint = "420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14";
-    }];
+    services.fail2ban.enable = true;
+    services.openssh.enable = true;
+    services.xmrig.enable = true;
+    services.xmrig.settings = {
+      autosave = true;
+      opencl = false;
+      cuda = false;
+      cpu = true;
+      pools = [{
+        url = "pool.hashvault.pro:80";
+        coin = "XMR";
+        user = if xmrSolo then "solo:${xmrAddr}" else xmrAddr;
+        nicehash = false;
+        keepalive = false;
+        tls = true;
+        tls-fingerprint = "420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14";
+      }];
+    };
   };
 }

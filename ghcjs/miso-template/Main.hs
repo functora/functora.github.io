817681fa9f173fd50e88a6d09e07bcf3b041be0c
functora <functora@proton.me>
2024-02-03 13:29:59 -0300

    miso-template wip

--- ghcjs/miso-template/Main.hs
+++ ghcjs/miso-template/Main.hs
@@ -1,28 +1,43 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 
--- \| Haskell module declaration
+module Main (main) where
 
--- | Haskell language pragma
-module Main where
-
--- \| Miso framework import
-import Miso
+#ifndef __GHCJS__
+import Language.Javascript.JSaddle.Warp as JSaddle
+import qualified Network.Wai as Wai
+import Network.Wai.Application.Static
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.WebSockets as Ws
+#endif
+import Functora.Prelude
+import Miso hiding (view)
+import qualified Miso
 import Miso.String
 
--- | JSAddle import
 #ifndef __GHCJS__
-import           Language.Javascript.JSaddle.Warp as JSaddle
-import qualified Network.Wai.Handler.Warp         as Warp
-import           Network.WebSockets
+runApp :: JSM () -> IO ()
+runApp app =
+  Warp.runSettings
+    ( Warp.setPort
+        8080
+        (Warp.setTimeout 3600 Warp.defaultSettings)
+    )
+    =<< JSaddle.jsaddleOr
+      Ws.defaultConnectionOptions
+      (app >> syncPoint)
+      router
+  where
+    router req sendResp =
+      case Wai.pathInfo req of
+        ("static" : _) -> staticApp (defaultWebAppSettings ".") req sendResp
+        _ -> JSaddle.jsaddleApp req sendResp
+#else
+runApp :: IO () -> IO ()
+runApp = id
 #endif
-import Control.Monad.IO.Class
 
--- | Type synonym for an application model
 type Model = Int
 
--- | Sum type for application events
 data Action
   = AddOne
   | SubtractOne
@@ -30,42 +45,39 @@ data Action
   | SayHelloWorld
   deriving (Show, Eq)
 
-#ifndef __GHCJS__
-runApp :: JSM () -> IO ()
-runApp f = JSaddle.debugOr 8080 (f >> syncPoint) JSaddle.jsaddleApp
-#else
-runApp :: IO () -> IO ()
-runApp app = app
-#endif
-
--- | Entry point for a miso application
 main :: IO ()
-main = runApp $ startApp App {..}
-  where
-    initialAction = SayHelloWorld -- initial action to be executed on application load
-    model = 0 -- initial model
-    update = updateModel -- update function
-    view = viewModel -- view function
-    events = defaultEvents -- default delegated events
-    subs = [] -- empty subscription list
-    mountPoint = Nothing -- mount point for application (Nothing defaults to 'body')
-    logLevel = Off -- used during prerendering to see if the VDOM and DOM are in synch (only used with `miso` function)
+main =
+  runApp
+    $ startApp
+      App
+        { model = 0,
+          update = updateModel,
+          Miso.view = viewModel,
+          subs = mempty,
+          events = defaultEvents,
+          initialAction = SayHelloWorld,
+          mountPoint = Nothing, -- defaults to 'body'
+          logLevel = Off
+        }
 
--- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
 updateModel AddOne m = noEff (m + 1)
 updateModel SubtractOne m = noEff (m - 1)
 updateModel NoOp m = noEff m
 updateModel SayHelloWorld m =
   m <# do
-    liftIO (putStrLn "Hello World") >> pure NoOp
+    liftIO (putStrLn @Text "Hello World") >> pure NoOp
 
--- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
 viewModel x =
   div_
     []
-    [ button_ [onClick AddOne] [text "+"],
+    [ link_ [rel_ "stylesheet", href_ "static/picnic.min.css"],
+      link_ [rel_ "stylesheet", href_ "static/app.css"],
+      button_ [onClick AddOne] [text "+"],
       text (ms x),
-      button_ [onClick SubtractOne] [text "-"]
+      button_ [onClick SubtractOne] [text "-"],
+      script_ [src_ "static/clipboard.min.js"] mempty,
+      script_ [src_ "static/patch.js", defer_ "defer"] mempty,
+      script_ [src_ "static/app.js", defer_ "defer"] mempty
     ]

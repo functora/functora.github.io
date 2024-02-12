f7ae69fffd41bff2364d3707a549de1d4cad6595
functora <functora@proton.me>
2024-02-12 12:04:50 -0300

    miso-components wip

--- ghcjs/miso-components/default.nix
+++ ghcjs/miso-components/default.nix
@@ -1,15 +1,11 @@
 let
   functora = ../..;
-  functora-miso = import "${functora}/ghcjs/miso/default.nix" {
-    # overlays = import ./overlays.nix {
-    #   inherit functora;
-    # };
-  };
+  functora-miso = import "${functora}/ghcjs/miso/default.nix" {};
 in
   with functora-miso; {
     inherit pkgs;
-    dev = pkgs.haskell.packages.ghc865.callCabal2nix "app" ./. {
+    dev = pkgs.haskell.packages.ghc865.callCabal2nix "miso-components" ./. {
       miso = miso-jsaddle;
     };
-    app = pkgs.haskell.packages.ghcjs86.callCabal2nix "app" ./. {};
+    app = pkgs.haskell.packages.ghcjs86.callCabal2nix "miso-components" ./. {};
   }
--- ghcjs/miso-components/shell.nix
+++ ghcjs/miso-components/shell.nix
@@ -1,6 +1,6 @@
 with (import ./default.nix); let
   app-ghcid = pkgs.writeScriptBin "app-ghcid" ''
-    (cd ${builtins.toString ./.} && ${pkgs.ghcid}/bin/ghcid --test=":main" --command="${pkgs.haskell.packages.ghc865.cabal-install}/bin/cabal new-repl app --disable-optimization --repl-options=-fobject-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j -fghcid")
+    (cd ${builtins.toString ./.} && ${pkgs.ghcid}/bin/ghcid --test=":main" --command="${pkgs.haskell.packages.ghc865.cabal-install}/bin/cabal new-repl miso-components-example --disable-optimization --repl-options=-fobject-code --repl-options=-fno-break-on-exception --repl-options=-fno-break-on-error --repl-options=-v1 --repl-options=-ferror-spans --repl-options=-j -fghcid")
   '';
   app-repl = pkgs.writeScriptBin "app-repl" ''
     (cd ${builtins.toString ./.} && ${pkgs.haskell.packages.ghc865.cabal-install}/bin/cabal new-repl)

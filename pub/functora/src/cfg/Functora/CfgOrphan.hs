03df5e6d39d0fdc31a53cc350e1125118b4a12ca
functora <functora@proton.me>
2024-07-24 16:11:19 -0300

    Aeson instances for URI

--- pub/functora/src/cfg/Functora/CfgOrphan.hs
+++ pub/functora/src/cfg/Functora/CfgOrphan.hs
@@ -2,14 +2,26 @@
 
 module Functora.CfgOrphan () where
 
+import qualified Data.Aeson as A
 import Functora.Prelude
 import qualified Text.URI as URI
 import qualified Toml
 
+instance A.ToJSON URI where
+  toJSON =
+    A.toJSON . URI.render
+
+instance A.FromJSON URI where
+  parseJSON =
+    A.withText "URI"
+      $ either (fail . displayException) pure
+      . URI.mkURI
+
 instance Toml.HasCodec URI where
   hasCodec =
-    Toml.textBy URI.render $
-      first (from @String @Text . displayException) . URI.mkURI
+    Toml.textBy URI.render
+      $ first (from @String @Text . displayException)
+      . URI.mkURI
 
 instance Toml.HasItemCodec URI where
   hasItemCodec =
@@ -21,4 +33,5 @@ instance Toml.HasItemCodec URI where
               (const . Toml.MatchError Toml.TText $ Toml.AnyValue src)
               $ URI.mkURI txt
         )
-      $ Toml.Text . URI.render
+      $ Toml.Text
+      . URI.render

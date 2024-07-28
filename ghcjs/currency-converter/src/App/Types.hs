557fcb8d7e5481638e66cef32795131f54a439ca
functora <functora@proton.me>
2024-07-28 18:57:35 -0300

    WIP

--- ghcjs/currency-converter/src/App/Types.hs
+++ ghcjs/currency-converter/src/App/Types.hs
@@ -3,52 +3,15 @@
 
 module App.Types
   ( Model (..),
-    Unique (..),
     Action (..),
     St (..),
     StConv (..),
     StDoc (..),
     StExt (..),
-    Money (..),
-    Currency (..),
-    PaymentMethod (..),
-    ChanItem (..),
     Screen (..),
     isQrCode,
     unQrCode,
-    Asset (..),
-    Fav (..),
-    TopOrBottom (..),
-    HeaderOrFooter (..),
-    OnlineOrOffline (..),
-    StaticOrDynamic (..),
-    LeadingOrTrailing (..),
-    FilledOrOutlined (..),
-    OpenedOrClosed (..),
-    AssetsAndPaymentsLayout (..),
-    newUnique,
-    newUniqueDuplicator,
     pureUpdate,
-    newAsset,
-    newPaymentMethod,
-    uniqueToIdentity,
-    identityToUnique,
-    FieldType (..),
-    htmlFieldType,
-    userFieldType,
-    DynamicField (..),
-    parseDynamicField,
-    inspectDynamicField,
-    Field (..),
-    newField,
-    newRatioField,
-    newTextField,
-    newDynamicField,
-    newDynamicTitleField,
-    newPasswordField,
-    newMoney,
-    FieldPair (..),
-    newFieldPair,
     unShareUri,
     stUri,
     stExtUri,
@@ -58,18 +21,19 @@ module App.Types
     setExtScreenAction,
     shareLink,
     vsn,
+    module X,
   )
 where
 
 import qualified Data.ByteString.Base64.URL as B64URL
 import qualified Data.ByteString.Lazy as BL
 import Data.Functor.Barbie
-import qualified Data.Generics as Syb
 import qualified Data.Text as T
 import qualified Data.Version as Version
 import qualified Functora.Aes as Aes
 import Functora.Cfg
 import Functora.Miso.Prelude
+import Functora.Miso.Types as X
 import Functora.Money hiding (Currency, Money, Text)
 import qualified Functora.Prelude as Prelude
 import Functora.Rates
@@ -94,12 +58,6 @@ data Model = Model
   }
   deriving stock (Eq, Generic)
 
-data Unique a = Unique
-  { uniqueUid :: Uid,
-    uniqueValue :: a
-  }
-  deriving stock (Eq, Ord, Show, Data, Generic)
-
 data Action
   = Noop
   | InitUpdate (Maybe (StExt Unique))
@@ -107,23 +65,6 @@ data Action
   | SyncInputs
   | ChanUpdate Model
   | PushUpdate (JSM (ChanItem (Model -> Model)))
-
-type Typ a =
-  ( Typeable a,
-    Eq a,
-    Ord a,
-    Show a,
-    Data a,
-    Binary a
-  )
-
-type Hkt f =
-  ( Typeable f,
-    Eq (f MisoString),
-    Ord (f MisoString),
-    Show (f MisoString),
-    Data (f MisoString)
-  )
 
 data St f = St
   { stScreen :: Screen,
@@ -219,81 +160,6 @@ instance TraversableB StDoc
 
 deriving via GenericType (StDoc Identity) instance Binary (StDoc Identity)
 
-data Money f = Money
-  { moneyAmount :: Field Rational f,
-    moneyCurrency :: Currency f
-  }
-  deriving stock (Generic)
-
-deriving stock instance (Hkt f) => Eq (Money f)
-
-deriving stock instance (Hkt f) => Ord (Money f)
-
-deriving stock instance (Hkt f) => Show (Money f)
-
-deriving stock instance (Hkt f) => Data (Money f)
-
-instance FunctorB Money
-
-instance TraversableB Money
-
-deriving via GenericType (Money Identity) instance Binary (Money Identity)
-
-data Currency f = Currency
-  { currencyInput :: Field MisoString f,
-    currencyOutput :: CurrencyInfo,
-    currencyModalState :: OpenedOrClosed
-  }
-  deriving stock (Generic)
-
-deriving stock instance (Hkt f) => Eq (Currency f)
-
-deriving stock instance (Hkt f) => Ord (Currency f)
-
-deriving stock instance (Hkt f) => Show (Currency f)
-
-deriving stock instance (Hkt f) => Data (Currency f)
-
-instance FunctorB Currency
-
-instance TraversableB Currency
-
-deriving via
-  GenericType (Currency Identity)
-  instance
-    Binary (Currency Identity)
-
-data PaymentMethod f = PaymentMethod
-  { paymentMethodMoney :: Money f,
-    paymentMethodMoneyLabel :: Field MisoString f,
-    paymentMethodFieldPairs :: [FieldPair DynamicField f],
-    paymentMethodModalState :: OpenedOrClosed
-  }
-  deriving stock (Generic)
-
-deriving stock instance (Hkt f) => Eq (PaymentMethod f)
-
-deriving stock instance (Hkt f) => Ord (PaymentMethod f)
-
-deriving stock instance (Hkt f) => Show (PaymentMethod f)
-
-deriving stock instance (Hkt f) => Data (PaymentMethod f)
-
-instance FunctorB PaymentMethod
-
-instance TraversableB PaymentMethod
-
-deriving via
-  GenericType (PaymentMethod Identity)
-  instance
-    Binary (PaymentMethod Identity)
-
-data ChanItem a = ChanItem
-  { chanItemDelay :: Natural,
-    chanItemValue :: a
-  }
-  deriving stock (Eq, Ord, Show, Data, Generic)
-
 data Screen
   = Converter
   | QrCode Screen
@@ -310,125 +176,6 @@ unQrCode = \case
   QrCode sc -> unQrCode sc
   sc -> sc
 
-data TopOrBottom
-  = Top
-  | Bottom
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType TopOrBottom
-
-data HeaderOrFooter
-  = Header
-  | Footer
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType HeaderOrFooter
-
-data OnlineOrOffline
-  = Online
-  | Offline
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType OnlineOrOffline
-
-data StaticOrDynamic
-  = Static
-  | Dynamic
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType StaticOrDynamic
-
-data LeadingOrTrailing
-  = Leading
-  | Trailing
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType LeadingOrTrailing
-
-data FilledOrOutlined
-  = Filled
-  | Outlined
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType FilledOrOutlined
-
-data OpenedOrClosed
-  = Opened
-  | Closed
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType OpenedOrClosed
-
-data AssetsAndPaymentsLayout
-  = AssetsBeforePayments
-  | PaymentsBeforeAssets
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType AssetsAndPaymentsLayout
-
-data Asset f = Asset
-  { assetPrice :: Money f,
-    assetPriceLabel :: Field MisoString f,
-    assetFieldPairs :: [FieldPair DynamicField f],
-    assetModalState :: OpenedOrClosed
-  }
-  deriving stock (Generic)
-
-deriving stock instance (Hkt f) => Eq (Asset f)
-
-deriving stock instance (Hkt f) => Ord (Asset f)
-
-deriving stock instance (Hkt f) => Show (Asset f)
-
-deriving stock instance (Hkt f) => Data (Asset f)
-
-instance FunctorB Asset
-
-instance TraversableB Asset
-
-deriving via GenericType (Asset Identity) instance Binary (Asset Identity)
-
-data Fav = Fav
-  { favUri :: URI,
-    favCreatedAt :: UTCTime
-  }
-  deriving stock (Eq, Ord, Show, Data, Generic)
-  deriving (ToJSON, FromJSON) via GenericType Fav
-
-newRatioField :: (MonadIO m) => Rational -> m (Field Rational Unique)
-newRatioField output =
-  newField FieldTypeNumber output inspectRatioDef
-
-newTextField :: (MonadIO m) => MisoString -> m (Field MisoString Unique)
-newTextField output =
-  newField FieldTypeText output id
-
-newDynamicField :: (MonadIO m) => DynamicField -> m (Field DynamicField Unique)
-newDynamicField output =
-  newField
-    ( case output of
-        DynamicFieldNumber {} -> FieldTypeNumber
-        DynamicFieldText {} -> FieldTypeText
-    )
-    output
-    inspectDynamicField
-
-newDynamicTitleField ::
-  (MonadIO m) => MisoString -> m (Field DynamicField Unique)
-newDynamicTitleField =
-  fmap (& #fieldType .~ FieldTypeTitle)
-    . newDynamicField
-    . DynamicFieldText
-
-newPasswordField :: (MonadIO m) => MisoString -> m (Field MisoString Unique)
-newPasswordField output =
-  newField FieldTypePassword output id
-
-newCurrency :: (MonadIO m) => CurrencyInfo -> m (Currency Unique)
-newCurrency cur =
-  Currency
-    <$> newTextField mempty
-    <*> pure cur
-    <*> pure Closed
-
-newMoney :: (MonadIO m) => Rational -> CurrencyInfo -> m (Money Unique)
-newMoney amt cur =
-  Money
-    <$> newRatioField amt
-    <*> newCurrency cur
-
 --
 -- NOTE : In most cases we don't need JSM.
 --
@@ -437,195 +184,6 @@ pureUpdate delay =
   PushUpdate
     . pure
     . ChanItem delay
-
-newUnique :: (MonadIO m) => a -> m (Unique a)
-newUnique x =
-  Unique
-    <$> newUid
-    <*> pure x
-
-newUniqueDuplicator ::
-  forall (b :: Type) a m.
-  ( Data a,
-    Typeable b,
-    MonadIO m
-  ) =>
-  m (a -> a)
-newUniqueDuplicator = do
-  uid <- newUid
-  pure
-    $ Syb.everywhere
-    $ Syb.mkT
-      ( (& #uniqueUid %~ addUid uid) :: Unique b -> Unique b
-      )
-
-newAsset ::
-  ( MonadIO m
-  ) =>
-  MisoString ->
-  Rational ->
-  CurrencyInfo ->
-  m (Asset Unique)
-newAsset label amt cur = do
-  lbl <- newTextField label
-  Asset
-    <$> newMoney amt cur
-    <*> pure lbl
-    <*> pure mempty
-    <*> pure Closed
-
-newPaymentMethod ::
-  ( MonadIO m
-  ) =>
-  CurrencyInfo ->
-  Maybe MisoString ->
-  m (PaymentMethod Unique)
-newPaymentMethod cur addr0 = do
-  lbl <- newTextField $ inspectCurrencyInfo cur <> " total"
-  addr1 <-
-    maybe
-      ( pure Nothing
-      )
-      ( fmap (Just . (& #fieldPairValue . #fieldType .~ FieldTypeQrCode))
-          . newFieldPair (inspectCurrencyInfo cur <> " address")
-          . DynamicFieldText
-      )
-      addr0
-  PaymentMethod
-    <$> newMoney 0 cur
-    <*> pure lbl
-    <*> pure (maybeToList addr1)
-    <*> pure Closed
-
-uniqueToIdentity :: (FunctorB f) => f Unique -> f Identity
-uniqueToIdentity =
-  bmap $ Identity . uniqueValue
-
-identityToUnique :: (TraversableB f, MonadIO m) => f Identity -> m (f Unique)
-identityToUnique =
-  btraverse $ newUnique . runIdentity
-
-data FieldType
-  = -- Rational
-    FieldTypeNumber
-  | FieldTypePercent
-  | -- Textual
-    FieldTypeText
-  | FieldTypeTitle
-  | FieldTypeQrCode
-  | FieldTypeHtml
-  | FieldTypePassword
-  deriving stock (Eq, Ord, Show, Enum, Bounded, Data, Generic)
-  deriving (Binary) via GenericType FieldType
-
-htmlFieldType :: FieldType -> MisoString
-htmlFieldType = \case
-  FieldTypeNumber -> "number"
-  FieldTypePercent -> "number"
-  FieldTypeText -> "text"
-  FieldTypeTitle -> "text"
-  FieldTypeQrCode -> "text"
-  FieldTypeHtml -> "text"
-  FieldTypePassword -> "password"
-
-userFieldType :: FieldType -> MisoString
-userFieldType = \case
-  FieldTypeNumber -> "Number"
-  FieldTypePercent -> "Percent"
-  FieldTypeText -> "Text"
-  FieldTypeTitle -> "Title"
-  FieldTypeQrCode -> "QR code"
-  FieldTypeHtml -> "HTML"
-  FieldTypePassword -> "Password"
-
-data DynamicField
-  = DynamicFieldText MisoString
-  | DynamicFieldNumber Rational
-  deriving stock (Eq, Ord, Show, Data, Generic)
-  deriving (Binary) via GenericType DynamicField
-
-parseDynamicField :: Field DynamicField Unique -> Maybe DynamicField
-parseDynamicField value =
-  case value ^. #fieldType of
-    FieldTypeNumber -> DynamicFieldNumber <$> parseRatio input
-    FieldTypePercent -> DynamicFieldNumber <$> parseRatio input
-    _ -> Just $ DynamicFieldText input
-  where
-    input = value ^. #fieldInput . #uniqueValue
-
-inspectDynamicField :: DynamicField -> MisoString
-inspectDynamicField = \case
-  DynamicFieldText x -> x
-  DynamicFieldNumber x -> inspectRatioDef x
-
-data Field a f = Field
-  { fieldType :: FieldType,
-    fieldInput :: f MisoString,
-    fieldOutput :: a,
-    fieldAllowCopy :: Bool,
-    fieldModalState :: OpenedOrClosed
-  }
-  deriving stock (Generic)
-
-deriving stock instance (Typ a, Hkt f) => Eq (Field a f)
-
-deriving stock instance (Typ a, Hkt f) => Ord (Field a f)
-
-deriving stock instance (Typ a, Hkt f) => Show (Field a f)
-
-deriving stock instance (Typ a, Hkt f) => Data (Field a f)
-
-instance FunctorB (Field a)
-
-instance TraversableB (Field a)
-
-deriving via
-  GenericType (Field a Identity)
-  instance
-    (Typ a) => Binary (Field a Identity)
-
-newField ::
-  (MonadIO m) => FieldType -> a -> (a -> MisoString) -> m (Field a Unique)
-newField typ output newInput = do
-  input <- newUnique $ newInput output
-  pure
-    Field
-      { fieldType = typ,
-        fieldInput = input,
-        fieldOutput = output,
-        fieldAllowCopy = True,
-        fieldModalState = Closed
-      }
-
-data FieldPair a f = FieldPair
-  { fieldPairKey :: Field MisoString f,
-    fieldPairValue :: Field a f
-  }
-  deriving stock (Generic)
-
-deriving stock instance (Typ a, Hkt f) => Eq (FieldPair a f)
-
-deriving stock instance (Typ a, Hkt f) => Ord (FieldPair a f)
-
-deriving stock instance (Typ a, Hkt f) => Show (FieldPair a f)
-
-deriving stock instance (Typ a, Hkt f) => Data (FieldPair a f)
-
-instance FunctorB (FieldPair a)
-
-instance TraversableB (FieldPair a)
-
-deriving via
-  GenericType (FieldPair a Identity)
-  instance
-    (Typ a) => Binary (FieldPair a Identity)
-
-newFieldPair ::
-  (MonadIO m) => MisoString -> DynamicField -> m (FieldPair DynamicField Unique)
-newFieldPair key val =
-  FieldPair
-    <$> newTextField key
-    <*> newDynamicField val
 
 unShareUri ::
   ( MonadThrow m,
@@ -640,10 +198,10 @@ unShareUri uri = do
   kPre <- URI.mkQueryKey "p"
   let qs = URI.uriQuery uri
   case (,,,)
-    <$> asumMap (qsGet kDoc) qs
-    <*> asumMap (qsGet kKm) qs
-    <*> asumMap (qsGet kSc) qs
-    <*> asumMap (qsGet kPre) qs of
+    <$> qsGet kDoc qs
+    <*> qsGet kKm qs
+    <*> qsGet kSc qs
+    <*> qsGet kPre qs of
     Nothing -> pure Nothing
     Just (vDoc, vKm, vSc, vPre) -> do
       bKm <- either throwString pure . B64URL.decode $ encodeUtf8 vKm
@@ -665,14 +223,6 @@ unShareUri uri = do
               stExtPre = pre,
               stExtScreen = sc
             }
-
-qsGet ::
-  URI.RText 'URI.QueryKey ->
-  URI.QueryParam ->
-  Maybe Prelude.Text
-qsGet qk = \case
-  URI.QueryParam k x | k == qk -> Just $ URI.unRText x
-  _ -> Nothing
 
 stQuery :: (MonadThrow m) => St Identity -> m [URI.QueryParam]
 stQuery st = do

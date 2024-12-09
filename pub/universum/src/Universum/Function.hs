55f662017800523fe2f8a666da14e188fcbe3c99
functora <functora@proton.me>
2024-12-09 15:45:47 -0300

    remove universum fork

--- pub/universum/src/Universum/Function.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE Safe #-}
-
--- | This module reexports very basic and primitive functions and function combinators.
-
-module Universum.Function
-       ( module Data.Function
-       ) where
-
-import Data.Function (const, fix, flip, id, on, ($), (.), (&))
--- pub/universum/test/Spec.hs
+++
@@ -1,9 +0,0 @@
-module Main
-  ( main
-  ) where
-
-import Test.Tasty
-import Tree (tests)
-
-main :: IO ()
-main = tests >>= defaultMain

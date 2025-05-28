d386fe1c4a32df3a90d1b22b6c8b41c2587b7a2d
functora <functora@proton.me>
2025-05-28 00:04:24 -0300

    stripe format

--- pub/stripe-hs/src/Stripe/Client/Internal/Helpers.hs
+++ pub/stripe-hs/src/Stripe/Client/Internal/Helpers.hs
@@ -1,25 +1,29 @@
--- | Private helper functions. Note that all contents of this module are excluded from the versioning scheme.
 {-# LANGUAGE BangPatterns #-}
+
+-- | Private helper functions. Note that all contents of this module are excluded from the versioning scheme.
 module Stripe.Client.Internal.Helpers where
 
-import Servant.Client
 import Network.HTTP.Types.Status
+import Servant.Client
 
-runRequest :: Int -> Int -> IO (Either ClientError a) -> IO (Either ClientError a)
+runRequest ::
+  Int -> Int -> IO (Either ClientError a) -> IO (Either ClientError a)
 runRequest maxRetries !retryCount makeRequest =
-  do res <- makeRequest
-     case res of
-       Right ok -> pure (Right ok)
-       Left err@(ConnectionError _) -> maybeRetry err
-       Left err@(FailureResponse _ resp)
-         | ("stripe-should-retry", "true") `elem` responseHeaders resp -> maybeRetry err
-         | ("stripe-should-retry", "false") `elem` responseHeaders resp -> pure (Left err)
-         | responseStatusCode resp == conflict409 -> maybeRetry err
-         | statusCode (responseStatusCode resp) >= 500 -> maybeRetry err
-         | otherwise -> pure (Left err)
-       Left err -> pure (Left err)
+  do
+    res <- makeRequest
+    case res of
+      Right ok -> pure (Right ok)
+      Left err@(ConnectionError _) -> maybeRetry err
+      Left err@(FailureResponse _ resp)
+        | ("stripe-should-retry", "true") `elem` responseHeaders resp -> maybeRetry err
+        | ("stripe-should-retry", "false") `elem` responseHeaders resp ->
+            pure (Left err)
+        | responseStatusCode resp == conflict409 -> maybeRetry err
+        | statusCode (responseStatusCode resp) >= 500 -> maybeRetry err
+        | otherwise -> pure (Left err)
+      Left err -> pure (Left err)
   where
     maybeRetry err =
       if retryCount + 1 >= maxRetries
-      then pure (Left err)
-      else runRequest maxRetries (retryCount + 1) makeRequest
+        then pure (Left err)
+        else runRequest maxRetries (retryCount + 1) makeRequest
--- pub/stripe-hs/src/Stripe/Webhook/Verify.hs
+++ pub/stripe-hs/src/Stripe/Webhook/Verify.hs
@@ -1,6 +1,7 @@
 module Stripe.Webhook.Verify
-  ( verifyStripeSignature
-  , WebhookSecret, VerificationResult(..)
+  ( verifyStripeSignature,
+    WebhookSecret,
+    VerificationResult (..),
   )
 where
 
@@ -8,48 +9,49 @@ import Crypto.Hash.Algorithms
 import Crypto.MAC.HMAC
 import Data.Bifunctor
 import Data.ByteArray.Encoding
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 import Data.Time
 import Data.Time.Clock.POSIX
 import Safe
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
 
 -- | Your webhook secret, can be obtained from the Stripe dashboard. Format: @whsec_<redacted>@
 type WebhookSecret = BS.ByteString
 
 -- | Output of the webhook signature verification
 data VerificationResult
-  = VOk UTCTime
-  -- ^ Signature verification successful, check the time against the current time and reject /too old/ requests.
-  | VFailed
-  -- ^ Signature verification failed. Check that your 'WebhookSecret' is correct.
-  | VInvalidSignature
-  -- ^ Invalid signature. Verify that you are passing the raw contents of the @stripe-signature@ header.
+  = -- | Signature verification successful, check the time against the current time and reject /too old/ requests.
+    VOk UTCTime
+  | -- | Signature verification failed. Check that your 'WebhookSecret' is correct.
+    VFailed
+  | -- | Invalid signature. Verify that you are passing the raw contents of the @stripe-signature@ header.
+    VInvalidSignature
   deriving (Show, Eq)
 
 -- | Verify the @stripe-signature@ header
 verifyStripeSignature ::
-  WebhookSecret
-  -- ^ Your webhook secret
-  -> BS.ByteString
-  -- ^ Value of the @stripe-signature@ header
-  -> BS.ByteString
-  -- ^ Raw request body received from Stripe
-  -> VerificationResult
+  -- | Your webhook secret
+  WebhookSecret ->
+  -- | Value of the @stripe-signature@ header
+  BS.ByteString ->
+  -- | Raw request body received from Stripe
+  BS.ByteString ->
+  VerificationResult
 verifyStripeSignature secret sig rawBody =
   let sigMap = map (second (BS.drop 1) . BSC.break (\c -> c == '=')) . BSC.split ',' $ sig
       needed =
-        do t <- lookup "t" sigMap
-           (parsedTime :: Int) <- readMay (BSC.unpack t)
-           v1 <- lookup "v1" sigMap
-           pure (t, posixSecondsToUTCTime $ fromIntegral parsedTime, v1)
-  in case needed of
-       Nothing -> VInvalidSignature
-       Just (rawTime, time, v1) ->
-         let payload = rawTime <> BSC.singleton '.' <> rawBody
-             computedSig :: HMAC SHA256
-             computedSig = hmac secret payload
-             hexSig = convertToBase Base16 computedSig
-         in if hexSig == v1
-               then VOk time
-               else VFailed
+        do
+          t <- lookup "t" sigMap
+          (parsedTime :: Int) <- readMay (BSC.unpack t)
+          v1 <- lookup "v1" sigMap
+          pure (t, posixSecondsToUTCTime $ fromIntegral parsedTime, v1)
+   in case needed of
+        Nothing -> VInvalidSignature
+        Just (rawTime, time, v1) ->
+          let payload = rawTime <> BSC.singleton '.' <> rawBody
+              computedSig :: HMAC SHA256
+              computedSig = hmac secret payload
+              hexSig = convertToBase Base16 computedSig
+           in if hexSig == v1
+                then VOk time
+                else VFailed

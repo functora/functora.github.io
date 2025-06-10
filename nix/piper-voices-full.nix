7fd910682f20f347770826d17ea21c5f180f02a2
functora <functora@proton.me>
2025-06-10 16:56:40 -0300

    rm et voice model

--- nix/piper-voices-full.nix
+++ nix/piper-voices-full.nix
@@ -6,12 +6,12 @@ let
     sha256 = "sha256-sldoRdODmy1HAfA2y0ozonjq3s63IZSvpkKoMHhXvO0=";
     fetchLFS = true;
   };
-  et = pkgs.fetchgit {
-    url = "https://huggingface.co/csukuangfj/vits-coqui-et-cv";
-    rev = "2e5249ce7a605279a5cb9cf70cc6df7ab801a55a";
-    sha256 = "sha256-GdsxCmoZ1PUqL8QeOCey9XCpXMAW7U41xt5x0h/fIgA=";
-    fetchLFS = true;
-  };
+  # et = pkgs.fetchgit {
+  #   url = "https://huggingface.co/csukuangfj/vits-coqui-et-cv";
+  #   rev = "2e5249ce7a605279a5cb9cf70cc6df7ab801a55a";
+  #   sha256 = "sha256-GdsxCmoZ1PUqL8QeOCey9XCpXMAW7U41xt5x0h/fIgA=";
+  #   fetchLFS = true;
+  # };
 in
   pkgs.stdenv.mkDerivation {
     name = "piper-voices-full";
@@ -20,8 +20,11 @@ in
     installPhase = ''
       mkdir -p $out
       cp -R ${pips}/* $out
-      mkdir -p $out/et/vits-coqui-et-cv
-      cp ${et}/model.onnx $out/et/vits-coqui-et-cv/model.onnx
-      cp ${et}/config.json $out/et/vits-coqui-et-cv/model.onnx.json
     '';
+    # mkdir -p $out/et/vits-coqui-et-cv
+    # cp ${et}/model.onnx $out/et/vits-coqui-et-cv/model.onnx
+    # ${pkgs.gnused}/bin/sed \
+    #   -E 's/\b-?Infinity\b/null/g; s/\bNaN\b/null/g' \
+    #   ${et}/config.json > \
+    #   $out/et/vits-coqui-et-cv/model.onnx.json
   }

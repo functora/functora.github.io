6d14583216f7c47446c9875c9504b6767baa8dff
functora <functora@proton.me>
2025-09-21 21:08:49 -0300

    rust shell

---
+++ rust/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Rust Dev Shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.rustup
+          ];
+        };
+      }
+    );
+}

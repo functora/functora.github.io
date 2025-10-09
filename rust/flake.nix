a02d83470fa529b424511dca06246fbd70249c38
functora <functora@proton.me>
2025-10-09 16:59:12 -0300

    rustell.nvim plugin wip

--- rust/flake.nix
+++ rust/flake.nix
@@ -47,8 +47,58 @@
               mkdir -p $out/lib
               cp target/${buildTarget}/release/*.wasm $out/lib/
             '';
-            # Disable checks if they only work for WASM
-            # doCheck = false;
+          };
+        mkRustellNvim = exe:
+          pkgs.vimUtils.buildVimPlugin {
+            name = "rustell-nvim";
+            src = pkgs.writeTextDir "plugin/rustell.vim" ''
+              augroup rust.vim.PreWrite
+                autocmd!
+              augroup END
+
+              augroup rustell_PreWrite
+                autocmd!
+                autocmd BufWritePre *.rs call s:RustellPreWrite()
+              augroup END
+
+              function! s:RustellPreWrite()
+                if !filereadable(expand('%'))
+                  return
+                endif
+
+                " Read buffer
+                let l:input = join(getline(1, '$'), "\n")
+
+                " --- run rustell first ---
+                let l:rustell_out = system('${exe}', l:input)
+                if v:shell_error
+                  echohl ErrorMsg | echom 'rustell failed: ' . l:rustell_out | echohl None
+                  return
+                endif
+
+                " --- now feed result into rustfmt ---
+                let l:rustfmt_cmd = 'rustfmt'
+                if exists('g:rustfmt_command')
+                  let l:rustfmt_cmd = g:rustfmt_command
+                endif
+                if exists('g:rustfmt_options')
+                  let l:rustfmt_cmd .= ' ' . g:rustfmt_options
+                endif
+
+                let l:rustfmt_out = system(l:rustfmt_cmd, l:rustell_out)
+                if v:shell_error
+                  echohl ErrorMsg | echom 'rustfmt failed: ' . l:rustfmt_out | echohl None
+                  return
+                endif
+
+                " Replace buffer with formatted output
+                let l:out_lines = split(l:rustfmt_out, "\n")
+                call setline(1, l:out_lines)
+                if line('$') > len(l:out_lines)
+                  execute len(l:out_lines)+1 . ',$delete _'
+                endif
+              endfunction
+            '';
           };
       in {
         devShells.default = pkgs.mkShell {
@@ -64,9 +114,13 @@
             wasmtime
           ];
         };
-        packages = {
+        packages = rec {
           rustell = mkRustPkg "rustell";
+          rustell-nvim = mkRustellNvim "${rustell}/bin/rustell";
           rustell-wasm = mkRustPkgWasm "rustell";
+          rustell-wasm-nvim = mkRustellNvim "${
+            pkgs.wasmtime
+          }/bin/wasmtime ${rustell-wasm}/bin/rustell.wasm";
           default = self.packages.${system}.rustell;
         };
       }

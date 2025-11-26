af021815e26b827ae8589970fa5a0bbcd282d117
functora <functora@proton.me>
2025-11-26 15:33:21 -0300

    latest BD64-VoH

--- nix/doom.nix
+++ nix/doom.nix
@@ -14,9 +14,11 @@ in
       exe = ''
         ${unst.gzdoom}/bin/gzdoom \
           -iwad ${../bak/doom/freedoom-0.13.0/freedoom2.wad} \
-          -file ${../bak/doom/BD64-VoH_game_v1.4.2.pk3} \
-          -file ${../bak/doom/BD64-VoH_maps_v1.4.2.pk3} \
-          -file ${../bak/doom/BD64-VoH_MoreSFX.pk3} \
+          -file ${../bak/doom/64/BD64-VoH_game_v1.6.1.pk3} \
+          -file ${../bak/doom/64/BD64-VoH_maps_v1.6.1.pk3} \
+          -file ${../bak/doom/64/BD64-VoH_MoreSFX.pk3} \
+          -file ${../bak/doom/64/BD64_HeavyBackpack.pk3} \
+          -file ${../bak/doom/64/BrutalDoom64_Metal_Veteran_Of_Hell_Edition.pk3} \
           -file ${../bak/doom/relite_0.7.3b.pk3} \
           -file ${../bak/doom/SimpleSlots.1.1.pk7}
       '';

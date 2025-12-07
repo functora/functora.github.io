a861a23f404f482be5e57f791b1d91a3e1094e49
functora <functora@proton.me>
2025-12-07 13:55:49 -0300

    favicon

--- rust/cryptonote/src/main.rs
+++ rust/cryptonote/src/main.rs
@@ -25,7 +25,11 @@ enum Route {
         Privacy {},
 }
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
+const FAVICON_ICO: Asset = asset!("/assets/favicon/favicon.ico");
+const FAVICON_16: Asset = asset!("/assets/favicon/favicon-16x16.png");
+const FAVICON_32: Asset = asset!("/assets/favicon/favicon-32x32.png");
+const APPLE_TOUCH_ICON: Asset = asset!("/assets/favicon/apple-touch-icon.png");
+const WEB_MANIFEST: Asset = asset!("/assets/favicon/site.webmanifest");
 
 fn main() {
     dioxus::launch(App);
@@ -61,7 +65,11 @@ fn App() -> Element {
     use_context_provider(|| nav_state);
 
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "icon", r#type: "image/x-icon", href: FAVICON_ICO }
+        document::Link { rel: "icon", r#type: "image/png", sizes: "16x16", href: FAVICON_16 }
+        document::Link { rel: "icon", r#type: "image/png", sizes: "32x32", href: FAVICON_32 }
+        document::Link { rel: "apple-touch-icon", sizes: "180x180", href: APPLE_TOUCH_ICON }
+        document::Link { rel: "manifest", href: WEB_MANIFEST }
         document::Link { rel: "stylesheet", href: asset!("/assets/bare.min.css") }
         document::Link { rel: "stylesheet", href: asset!("/assets/app.css") }
         Router::<Route> {}

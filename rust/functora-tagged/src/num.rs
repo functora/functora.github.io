95189c39fa1b53dffd5887c324d33776fa1cc36d
functora <functora@proton.me>
2026-02-01 14:42:28 -0300

    functora-tagged num wip

--- rust/functora-tagged/src/num.rs
+++ rust/functora-tagged/src/num.rs
@@ -26,6 +26,149 @@ where
 }
 
 //
+// Add
+//
+
+pub trait FAdd
+where
+    Self: Sized + Debug,
+{
+    fn fadd(
+        &self,
+        rhs: &Self,
+    ) -> Result<Self, FNumError<Self, Self>>;
+}
+
+impl<Rep> FAdd for Rep
+where
+    Rep: Copy + Debug + CheckedAdd,
+{
+    fn fadd(
+        &self,
+        rhs: &Self,
+    ) -> Result<Self, FNumError<Rep, Rep>> {
+        self.checked_add(rhs)
+            .ok_or(FNumError::Add(*self, *rhs))
+    }
+}
+
+impl<Rep, Tag> FAdd for Tagged<Rep, Tag>
+where
+    Rep: Copy + Debug + FAdd,
+    Tag: Debug + Refine<Rep>,
+{
+    fn fadd(
+        &self,
+        rhs: &Self,
+    ) -> Result<Self, FNumError<Self, Self>> {
+        self.rep()
+            .fadd(rhs.rep())
+            .map_err(|_| FNumError::Add(*self, *rhs))?
+            .pipe(Self::new)
+            .map_err(|_| FNumError::Add(*self, *rhs))
+    }
+}
+
+//
+// Sub
+//
+
+pub trait FSub
+where
+    Self: Sized + Debug,
+{
+    fn fsub(
+        &self,
+        rhs: &Self,
+    ) -> Result<Self, FNumError<Self, Self>>;
+}
+
+impl<Rep> FSub for Rep
+where
+    Rep: Copy + Debug + CheckedSub,
+{
+    fn fsub(
+        &self,
+        rhs: &Self,
+    ) -> Result<Self, FNumError<Rep, Rep>> {
+        self.checked_sub(rhs)
+            .ok_or(FNumError::Sub(*self, *rhs))
+    }
+}
+
+impl<Rep, Tag> FSub for Tagged<Rep, Tag>
+where
+    Rep: Copy + Debug + FSub,
+    Tag: Debug + Refine<Rep>,
+{
+    fn fsub(
+        &self,
+        rhs: &Self,
+    ) -> Result<Self, FNumError<Self, Self>> {
+        self.rep()
+            .fsub(rhs.rep())
+            .map_err(|_| FNumError::Sub(*self, *rhs))?
+            .pipe(Self::new)
+            .map_err(|_| FNumError::Sub(*self, *rhs))
+    }
+}
+
+//
+// Mul
+//
+
+pub trait FMul<Rhs, Output>
+where
+    Self: Sized + Debug,
+    Rhs: Debug,
+{
+    fn fmul(
+        &self,
+        rhs: &Rhs,
+    ) -> Result<Output, FNumError<Self, Rhs>>;
+}
+
+impl<Rep> FMul<Rep, Rep> for Rep
+where
+    Rep: Copy + Debug + CheckedMul,
+{
+    fn fmul(
+        &self,
+        rhs: &Rep,
+    ) -> Result<Rep, FNumError<Rep, Rep>> {
+        self.checked_mul(rhs)
+            .ok_or(FNumError::Mul(*self, *rhs))
+    }
+}
+
+pub enum TimesTag {}
+pub type Times<LTag, RTag> = (TimesTag, LTag, RTag);
+
+impl<Rep, LTag, RTag>
+    FMul<Tagged<Rep, RTag>, Tagged<Rep, Times<LTag, RTag>>>
+    for Tagged<Rep, LTag>
+where
+    Rep: Copy + Debug + FMul<Rep, Rep>,
+    LTag: Debug,
+    RTag: Debug,
+    Times<LTag, RTag>: Refine<Rep>,
+{
+    fn fmul(
+        &self,
+        rhs: &Tagged<Rep, RTag>,
+    ) -> Result<
+        Tagged<Rep, Times<LTag, RTag>>,
+        FNumError<Tagged<Rep, LTag>, Tagged<Rep, RTag>>,
+    > {
+        self.rep()
+            .fmul(rhs.rep())
+            .map_err(|_| FNumError::Mul(*self, *rhs))?
+            .pipe(Tagged::new)
+            .map_err(|_| FNumError::Mul(*self, *rhs))
+    }
+}
+
+//
 // Div
 //
 
@@ -61,11 +204,8 @@ where
 {
     fn fdiv(
         &self,
-        rhs: &Tagged<Rep, Tag>,
-    ) -> Result<
-        Rep,
-        FNumError<Tagged<Rep, Tag>, Tagged<Rep, Tag>>,
-    > {
+        rhs: &Self,
+    ) -> Result<Rep, FNumError<Self, Self>> {
         self.rep()
             .fdiv(rhs.rep())
             .map_err(|_| FNumError::Div(*self, *rhs))

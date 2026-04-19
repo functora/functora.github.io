ba2983e006008d4e43040cdb73c00cefeeec39e7
functora <functora@proton.me>
2026-04-19 10:07:18 -0300

    better bubble.nix

--- nix/bubble.nix
+++ nix/bubble.nix
@@ -6,6 +6,7 @@
   user ? "bubble",
   pasta ? true,
   network ? false,
+  mkOverlay ? _: [], # Sloth -> [{ path :: [String]; update :: (Any -> Any); }]
 }: let
   nixpak = import ./nixpak.nix;
   mkNixPak = nixpak.lib.nixpak {
@@ -23,36 +24,47 @@
       else "/home/${user}"
     }:/bin/sh";
   };
+  asound = pkgs.writeText "asound.conf" ''
+    pcm.!default {
+      type pulse
+    }
+    ctl.!default {
+      type pulse
+    }
+  '';
   sandbox = mkNixPak {
-    config = {sloth, ...}: {
-      app.package = app;
-      gpu.enable = true;
-      fonts.enable = true;
-      pasta.enable = pasta;
-      locale.enable = true;
-      etc.sslCertificates.enable = network;
-      bubblewrap = {
-        inherit network;
-        dieWithParent = true;
-        sockets.pulse = true;
-        sockets.wayland = true;
-        bind.ro = [
-          [(toString passwd) "/etc/passwd"]
-        ];
-        bind.rw =
-          if home == null
-          then []
-          else [
-            [
-              (sloth.mkdir (sloth.concat' sloth.homeDir "/doom"))
-              sloth.homeDir
-            ]
+    config = {sloth, ...}:
+      pkgs.lib.updateManyAttrsByPath (mkOverlay sloth) {
+        app.package = app;
+        gpu.enable = true;
+        fonts.enable = true;
+        pasta.enable = pasta;
+        locale.enable = true;
+        etc.sslCertificates.enable = network;
+        bubblewrap = {
+          inherit network;
+          dieWithParent = true;
+          sockets.pulse = true;
+          sockets.wayland = true;
+          env.ALSA_PLUGIN_DIR = "${pkgs.alsa-plugins}/lib/alsa-lib";
+          bind.ro = [
+            [(toString passwd) "/etc/passwd"]
+            [(toString asound) "/etc/asound.conf"]
           ];
-        tmpfs = [
-          "/tmp"
-        ];
+          bind.rw =
+            if home == null
+            then []
+            else [
+              [
+                (sloth.mkdir (sloth.concat' sloth.homeDir home))
+                sloth.homeDir
+              ]
+            ];
+          tmpfs = [
+            "/tmp"
+          ];
+        };
       };
-    };
   };
 in
   sandbox.config.env

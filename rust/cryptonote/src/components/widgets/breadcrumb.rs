7a56c4b8287dbb7a151a56dd3a384e58415faa79
functora <functora@proton.me>
2026-06-12 19:07:58 -0300

    -

--- rust/cryptonote/src/components/widgets/breadcrumb.rs
+++ rust/cryptonote/src/components/widgets/breadcrumb.rs
@@ -13,7 +13,7 @@ pub fn Breadcrumb<T: I18N + Clone + PartialEq + 'static>(
         functora_dioxus::widgets::Breadcrumb {
             title,
             home_label: Msg::Home,
-            home_href: Screen::Home.to_route(None).to_string(),
+            home_route: Screen::Home.to_route(None),
             nav,
             lang,
         }
--- rust/functora-dioxus/src/widgets/breadcrumb.rs
+++ rust/functora-dioxus/src/widgets/breadcrumb.rs
@@ -5,13 +5,13 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Breadcrumb<
-    R: Routable + Default + PartialEq + 'static,
+    R: Routable + Clone + Default + PartialEq + 'static,
     T: I18N + Clone + PartialEq + 'static,
     U: I18N + Clone + PartialEq + 'static,
 >(
     title: T,
     home_label: U,
-    home_href: String,
+    home_route: R,
     nav: Signal<Nav<R>>,
     lang: Language,
 ) -> Element {
@@ -21,7 +21,7 @@ pub fn Breadcrumb<
                 href: "#",
                 onclick: move |evt| {
                     evt.prevent_default();
-                    nav.write().push_route(&home_href);
+                    nav.write().push(home_route.clone());
                 },
                 "{home_label.render(lang)}"
             }
